f0a7925122d883a2dd4a9c6686f863ec333ff986
Michael J. Pedersen <m.pedersen@icelus.org>
2012-04-26 00:44:29 -0400

    Upping to version 1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='tgext.xmlrpc',
-    version='0.8',
+    version='1.0',
     description='TurboGears XMLRPC Controller',
     author='Michael J. Pedersen',
     author_email='m.pedersen@icelus.org',
